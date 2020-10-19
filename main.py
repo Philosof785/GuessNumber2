c097ea24fe463ca7e39ade3182d97a488b1694b3
Philosof785 <kbaranov833@gmail.com>
2020-10-19 10:21:18 +1000

    Третье задание

--- main.py
+++ main.py
@@ -18,16 +18,17 @@ pygame.display.set_caption("Угадай число")
 screen = pygame.display.set_mode((W, H))
 pygame.mouse.set_visible(False)
 
-font = pygame.font.SysFont.("Arial", 28, True, False)
-font_box = pygame.Surface((W - font.get_height(), font.get_height()))
+font = pygame.font.SysFont("Arial", 28, True, False)
+font_box = pygame.Surface((W - 30, font.get_height()))
 font_rect = font_box.get_rect(center=(W // 2, H - font.get_height()))
-font2 = pygame.font.SysFont.("Arial", 14, False, True)
+font2 = pygame.font.SysFont("Arial", 14, False, True)
 
 bg = pygame.image.load("Image/room.png")
 bg_rect = bg.get_rect(topleft=(0, 0))
 
 cat = pygame.image.load("Image/cat.png")
 cat_rect = cat.get_rect(center=(70, 220))
+print(cat_rect)
 
 dog = pygame.image.load("Image/dog.png")
 dog_rect = dog.get_rect(center=(410, 220))
@@ -39,6 +40,16 @@ dialog = pygame.image.load("Image/dialog.png")
 dialog_rect = dialog.get_rect()
 dialog_cat_pos = (cat_rect.x, cat_rect.y - dialog_rect.h)
 dialog_dog_pos = (dog_rect.x - dialog_rect.w // 2, dog_rect.y - dialog_rect.h)
+dialog_owl_pos = (owl_rect.x, owl_rect.y - dialog_rect.h)
+
+
+def dialogs(text, pos, owl_text):
+    screen.blit(dialog, pos)
+    screen.blit(font2.render(text, True, BLACK), (pos[0] + 5, pos[1] + 5))
+    screen.blit(dialog, dialog_owl_pos)
+    dialog.blit(font2.render(owl_text, True, BLACK), (dialog_owl_pos[0] + 5, pos[1] + 5))
+    pygame.display.update()
+    pygame.time.wait(2000)
 
 run = True
 while run:
@@ -48,9 +59,22 @@ while run:
         elif e.type == pygame.KEYDOWN:
             if e.key == pygame.K_ESCAPE:
                 run = False 
-                
+            elif e.type == pygame.KEYDOWN:
+                run = False
+            elif e.unicode.isdecimal() and black == 0:
+                numeral += e.unicode
+
     screen.blit(bg, bg_rect)
     screen.blit(cat, cat_rect)
     screen.blit(dog, dog_rect)
     screen.blit(owl, owl_rect)
-    pygame.display.update()     
+    screen.blit(font_box, font_rect)
+    font_box.fill(SILVER)
+    font_box.blit(font.render(numeral, True, BLACK), (10, 1))
+    pygame.display.update()  
+    
+    if start == 1:
+        dialogs("", OUTSIDE_BG, "Я загадала число")
+        dialogs("", OUTSIDE_BG, "от 0 до 100")
+        dialogs("Кот твой ход", dialog_dog_pos, "Отгадайте его число")
+        start = 0
